40ab437dc5555d92d6078cebdd23337469f3aeb7
kikikaori <117708403+kikikaori@users.noreply.github.com>
2022-11-08 16:12:55 +0900

    ねこ追加

---
+++ cat/practice.js
@@ -0,0 +1,62 @@
+let img;
+let y = 0;
+let x = 0;
+let a;
+const cats = [];
+
+class Cat {
+    constructor (x,y){
+        this.x = x;
+        this.y = y;
+    }
+
+    move(){
+        this.x = this.x += 50*2;
+
+        if (this.x >= 400 && this.x == 400){
+                this.x = 50;
+                this.y = this.y += 50;
+            } if ( this.x >= 400 ) {
+                this.x = 0;
+                this.y = this.y += 50;
+            }
+            
+        }
+    
+
+    
+
+    draw(){
+        image(img,this.x,this.y);
+    }
+
+}
+
+
+
+function preload() {
+    
+    img = loadImage('cat.png');
+    
+}
+
+function setup(){
+    img.resize(50,50);
+    canvas = createCanvas(400,400);
+    
+}
+
+const cat = new Cat(0,0);
+cats.push(cat);
+
+function draw(){
+    for ( let a of cats){
+    a.draw();
+    a.move();}
+}
+
+
+
+
+
+
